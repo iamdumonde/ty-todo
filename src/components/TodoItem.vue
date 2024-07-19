3546a87100f872f0bee70d35cf53bfa45e4fa3b0
iamdumonde <martinaboumonde@gmail.com>
2024-07-19 11:30:48 +0100

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,89 @@
+<template>
+  <li :class="{ completed: todo.complete, editing: editing }">
+    <div class="view">
+      <input type="checkbox" class="toggle" v-model="isTodoCompleted" />
+      <label @dblclick="startEditing">{{ todo.title }}</label>
+      <button class="destroy" @click="emit('delete-todo', todo)"></button>
+      <!-- <p>{{ todo.complete }}</p> -->
+    </div>
+    <div class="input-container">
+      <input
+        ref="editRef"
+        id="edit-to-input"
+        class="edit"
+        type="text"
+        v-model="editInput"
+        @keyup.enter="finishEdit"
+        @blur="cancelEdit"
+      />
+      <label class="visually-hidden" for="edit-to-input">Editer</label>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { Todo } from '@/@types'
+import { computed, ref, watch } from 'vue'
+const props = defineProps<{
+  todo: Todo
+}>()
+
+const emit = defineEmits<{
+  (e: 'delete-todo', todo: Todo): void
+  (e: 'update-todo', todo: Todo, completeVal: boolean): void
+  (e: 'edit-todo', todo: Todo, value: string): void
+}>()
+
+const isTodoCompleted = ref<boolean>(props.todo.complete)
+
+watch(
+  () => isTodoCompleted.value,
+  (newVal) => {
+    emit('update-todo', props.todo, newVal)
+  }
+)
+
+const editing = ref<boolean>(false)
+const editText = ref<string>('')
+const editInput = computed({
+  get: () => props.todo.title,
+  set: (val) => {
+    editText.value = val
+  }
+})
+
+async function startEditing() {
+  editing.value = true
+}
+
+function finishEdit() {
+  editing.value = false
+
+  editTodo()
+}
+
+function editTodo() {
+  emit('edit-todo', props.todo, editText.value) // emettre un event
+
+  editText.value = ''
+}
+
+function cancelEdit() {
+  console.log('cancel edit')
+  editing.value = false
+}
+</script>
+
+<style scoped>
+.visually-hidden {
+  bottom: 0;
+  clip: rect(0 0 0 0);
+  clip-path: 50%;
+  height: 1px;
+  width: 1px;
+  margin: 1px;
+  padding: 0;
+  overflow: hidden;
+  white-space: wrap;
+}
+</style>
---
+++ src/components/TodosComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <TodoHeader @add-todo="addTodo" />
+
+    <TodoMain
+      :taches="todos"
+      @delete-todo="deleteTodo"
+      @update-todo="updateTodo"
+      @edit-todo="editTodo"
+    />
+
+    <TodoFooter :todos="todos" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TodoHeader from '@/components/TodoHeader.vue'
+import TodoMain from '@/components/TodoMain.vue'
+import TodoFooter from '@/components/TodoFooter.vue'
+import type { Todo } from '@/@types'
+import { ref } from 'vue'
+import { nanoid } from 'nanoid'
+import { useStorage } from '@vueuse/core'
+
+// const todos = ref<Todo[]>([])
+const todos = useStorage<Todo[]>('todoapp-todos', []);
+
+
+
+
+function addTodo(value: string): void {
+  if(value.trim().length === 0){
+    return; // Do nothing if the input is empty or whitespace.
+  }
+  todos.value.push({
+    id: nanoid(),
+    title: value,
+    complete: false
+  })
+}
+
+function deleteTodo(todo: Todo): void {
+  todos.value = todos.value.filter((t) => t !== todo)
+}
+
+function updateTodo(todo: Todo, completedValue: boolean) {
+  todo.complete = completedValue
+}
+
+function editTodo(todo: Todo, value: string) {
+  todo.title = value
+}
+</script>
+
+<style scoped></style>
