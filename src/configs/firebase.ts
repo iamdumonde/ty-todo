ed2aac269731e97500ed6fc05ffab1caafc0266d
iamdumonde <martinaboumonde@gmail.com>
2024-07-20 11:20:34 +0100

    ajout de firebase et des config d'authentification

---
+++ src/configs/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, type FirebaseOptions } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey:import.meta.env.VITE_API_KEY,
+  authDomain:import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:import.meta.env.VITE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_SSTORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId:import.meta.env.VITE_APP_ID,
+}
+
+// Initialize Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Initialize authentication service
+const auth = getAuth(app);
+
+export { auth };
